eb73200898cd17a8daa9de848320cd62b9d929bf
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2023-01-22 23:44:20 +0200

    add done on Task

--- lib/home.dart
+++ lib/home.dart
@@ -10,7 +10,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final List<String> tarefas = <String>['A', 'B'];
+  // final List<String> tarefas = <String>['A', 'B'];
+  bool done = false;
+
+  final List<Tarefa> tarefas = [
+    Tarefa(
+        nome: "PHP",
+        descricao: "Aprender a desenvolver aplicativos web usando PHP",
+        done: false),
+    Tarefa(
+      nome: "Laravel",
+      descricao: "Aprender a construir Rest API's",
+      done: true,
+    ),
+    Tarefa(
+        nome: "Flutter",
+        descricao: "Aprender a desenvolver aplicativos moveis",
+        done: false),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +35,24 @@ class _HomeState extends State<Home> {
         // leading: IconButton(),
         title: Text('Tarefas'),
       ),
-      body: ListView.builder(
-        itemCount: tarefas.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(tarefas[index]),
-          );
-        },
+      body: Padding(
+        padding: EdgeInsets.only(left: 10, right: 10, top: 20),
+        child: ListView.builder(
+          itemCount: tarefas.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(tarefas[index].nome),
+              subtitle: Text(tarefas[index].descricao),
+              trailing: Checkbox(
+                  value: tarefas[index].done,
+                  onChanged: (value) {
+                    setState(() {
+                      done = value!;
+                    });
+                  }),
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -34,4 +62,12 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+}
+
+class Tarefa {
+  String nome;
+  String descricao;
+  bool? done;
+
+  Tarefa({required this.nome, required this.descricao, this.done});
 }
