b9c7024b0ed43109c80e7b2e5b02050340bd6726
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2023-01-23 00:14:56 +0200

    splashpage done

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,10 +1,31 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
 
   @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.pink,
+      child: Center(
+        child: SizedBox(
+            height: 100,
+            width: 100,
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.done,
+                size: 70,
+              ),
+            )),
+      ),
+    );
   }
 }
