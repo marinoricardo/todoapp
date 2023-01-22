eda26b6a17ebd33feec7fc069ff6c81c2db82750
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2023-01-22 23:22:10 +0200

    create home

--- lib/home.dart
+++ lib/home.dart
@@ -2,9 +2,15 @@
 
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<String> tarefas = <String>['A', 'B'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,11 +18,20 @@ class Home extends StatelessWidget {
         // leading: IconButton(),
         title: Text('Tarefas'),
       ),
+      body: ListView.builder(
+        itemCount: tarefas.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(tarefas[index]),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.of(context).pushNamed('/addtask');
-          },
-          child: Icon(Icons.add)),
+        onPressed: () {
+          Navigator.of(context).pushNamed('/addtask');
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
